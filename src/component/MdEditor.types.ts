fcf8f2abfd123390f4edf4f7f7f09dd7bab66c47
Sachin-chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-04-06 18:52:44 +0530

    Remove unwanted props

--- src/component/MdEditor.types.ts
+++ src/component/MdEditor.types.ts
@@ -1,10 +1,6 @@
-export type List = Array<{ name: string; url: string }>;
 export type PreviewStyle = "tab" | "vertical";
 export type EditorType = "markdown" | "wysiwyg";
 export type MdEditorProps = {
-  mentionList: List;
-  mentionTrigger?: string;
-  hashTagTrigger?: string;
   placeHolder?: string;
   previewStyle?: PreviewStyle;
   editorType?: EditorType;
